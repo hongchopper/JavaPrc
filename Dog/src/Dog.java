e1ba1dec35abebb7c98683bf3ca714aee2b9e7ec
hongchopper <hongchopper@gmail.com>
2022-03-22 23:56:03 +0900

    강아지

---
+++ Dog/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog { //알고리즘에서는 Main이라고 하면되
+	public static void main(String args[]) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
