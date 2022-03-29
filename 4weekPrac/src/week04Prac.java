4cfce743158f1e28c3403c13f9afc9fa29f2a763
hongchopper <seonmihong69@gmail.com>
2022-03-30 00:20:09 +0900

    과제-week4

--- 4weekPrac/src/week04Prac.java
+++ 4weekPrac/src/week04Prac.java
@@ -1,4 +1,28 @@
-
-public class week04Prac {
-
+public class week04Prac { //Book이름대신
+	String title;
+	String author;
+	
+	public week04Prac() {
+		title="제목없음";
+		author="작자미상";
+	}
+	
+	public week04Prac(String t) {
+		t=title;
+		author="작자미상";
+	}
+	
+	public week04Prac(String t, String a) {
+		t=title;
+		a=author;
+	}
+	
+	public static void main() {
+		week04Prac noTitle = new week04Prac();
+		week04Prac littlePrince = new week04Prac("어린왕자","생텍쥐페리");
+		week04Prac loveStory = new week04Prac("춘향전");
+		System.out.println(noTitle+""+noTitle.author);
+		System.out.println(littlePrince+""+littlePrince.author);
+		System.out.println(loveStory+""+loveStory.author);
+	}
 }
